e1d1e23e7ec7642c5cfc80bf025832bc09c69f31
pamu47 <pamudithamaduranga@gmail.com>
2019-05-01 19:55:11 +0530

    routes added

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import logo from "./img/train-ticket.png"
 
+import MakeReservation from './components/make-reservation.component'
+import Home from './components/home.component'
+import Login from './components/login.component'
+import Register from './components/register.component'
+	
 class App extends Component {
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+          <img src={logo} width={40} height={40} alt=""/>&nbsp;&nbsp;&nbsp;&nbsp;</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link to="/" className="nav-link">Home <span className="sr-only">(current)</span></Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/create" className="nav-link">Reserve</Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/create" component={MakeReservation} />
+        <Route path="/signin" component={Login} />
+        <Route path="/signup" component={Register}/>
       </div>
+      </Router>
     );
   }
 }
