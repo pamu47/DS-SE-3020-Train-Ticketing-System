a576a3d48b65265a56a05721a860b200a4f3d80b
pamu47 <pamudithamaduranga@gmail.com>
2019-05-03 13:56:41 +0530

    logout function

--- webclient/src/components/home.component.js
+++ webclient/src/components/home.component.js
@@ -7,7 +7,27 @@ var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
 export default class Home extends Component{
 
+  logOut(e){
+    e.preventDefault()
+    localStorage.removeItem('usertoken')
+    this.props.history.push('/')
+  }
+
     render(){
+      const loginLink = (
+        <div>
+        <p>Please sign in  or sign up to proceed</p>
+        <Link to="/signin" class="btn btn-success" style={{marginRight:15}}>Sign in</Link>
+        <Link to="/signup" class="btn btn-info">Sign up</Link>
+        </div>
+      )
+
+      const reservationLink = (
+        <div>
+          <Link to="/create" class="btn btn-primary" style={{marginRight:15}}>Make a reservation</Link>
+          <button onClick={this.logOut.bind(this)} class="btn btn-danger">Logout</button>
+        </div>
+      )
         return(
             <div className="row" style={{marginTop:75}}>
             <div className="col-2"></div>
@@ -18,9 +38,7 @@ export default class Home extends Component{
                     </div>
                     <div class="card-body">
                       <h2 class="card-title">Train Ticket Reservation System</h2>
-                      <p>Please sign in  or sign up to proceed</p>
-                      <Link to="/signin" class="btn btn-success" style={{marginRight:15}}>Sign in</Link>
-                      <Link to="/signup" class="btn btn-info">Sign up</Link>
+                      {localStorage.usertoken ? reservationLink : loginLink}
                     </div>
                     <div class="card-footer text-muted" style={{backgroundColor:"LightGray"}}>
                       {date}
