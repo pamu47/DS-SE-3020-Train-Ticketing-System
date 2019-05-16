7de70fabfd6b00835faf3dcc7ceab0ebca89275f
pamu47 <pamudithamaduranga@gmail.com>
2019-05-16 20:47:12 +0530

    styles and js for NIC input

--- webclient/src/components/make-reservation.component.js
+++ webclient/src/components/make-reservation.component.js
@@ -1,7 +1,37 @@
 import React, {Component} from "react";
+import jwt_decode from 'jwt-decode'
+import '../App.css'
+
 
 
 export default class MakeReservation extends Component{
+    constructor(){
+        super()
+        this.state = {
+            full_name : '',
+            email : ''
+        }
+    }
+
+    onChange(e){
+        var radio = document.getElementsByName('optionRadios');
+        if (radio[0].checked){
+            document.getElementById('div1').style.display = 'block';
+        }else if(radio[1].checked){
+            document.getElementById('div1').style.display = 'none';
+        }
+        
+    }
+
+    // componentDidMount(){
+    //     const token = localStorage.usertoken
+    //     const decode = jwt_decode(token)
+    //     this.setState({
+    //         full_name : decode.full_name,
+    //         email : decode.email,
+    //         username : decode.username
+    //     })
+    // }
 
     render(){
         return(
@@ -34,13 +64,17 @@ export default class MakeReservation extends Component{
                                 </div>
                                 <div className="form-group">
                                     <div className="form-check form-check-inline">
-                                        <input className="form-check-input" type="radio" name="option" id="optionYes" value="Yes" />
+                                        <input className="form-check-input" type="radio" onChange={this.onChange} name="optionRadios" id="optionYes" value="Yes" />
                                         <label className="form-check-label" for="optionYes">Yes</label>
                                     </div>
                                     <div className="form-check form-check-inline">
-                                        <input className="form-check-input" type="radio" name="option" id="optionNo" value="No" />
+                                        <input className="form-check-input" type="radio" name="optionRadios" onChange={this.onChange} id="optionNo" value="No" />
                                         <label className="form-check-label" for="optionNo">No</label>
                                     </div>
+                                </div>
+                                <div className="form-group hide" id="div1">
+                                    <label>Please enter your NIC : </label>
+                                    <input className="form-control" type="text"/>
                                 </div>
                                 <div className="form-group">
                                     <input className="btn btn-success" type="submit" value="Proceed"/>
