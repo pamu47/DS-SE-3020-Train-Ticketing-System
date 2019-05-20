e13a2b05517a21a1123a1d9b8b5580f757e20d67
pamu47 <pamudithamaduranga@gmail.com>
2019-05-20 23:08:32 +0530

    twilio text msg function

--- server/routes/Reservations.js
+++ server/routes/Reservations.js
@@ -3,6 +3,11 @@ const reservations = express.Router()
 
 const Booking = require('../models/Reservation')
 
+//Setting up twilio for sending a text when a reservation occurs
+const accountSid = 'AC2285c402a8db5262edec637217f0a313';
+const authToken = 'bc13f91ff8eec6d701d229cbb4ff1257';
+const client = require('twilio')(accountSid, authToken);
+
 
 reservations.post('/', (req,res) => {
     const bookingData = {
@@ -16,6 +21,17 @@ reservations.post('/', (req,res) => {
 
     Booking.create(bookingData)
         .then(user => {
+            //Pre defined message when reservation success
+            const msg = 'Your reservation from '+bookingData.from+' to '+bookingData.to+' is successful.'
+            console.log(bookingData.tel_no)
+            client.messages
+            .create({
+               body: msg,
+               from: '+12016763615',
+               to: bookingData.tel_no
+             })
+            .then(message => console.log(message.sid));
+
             res.json({status : from + " to " + to + "train booked"})
         })
         .catch(err => {
